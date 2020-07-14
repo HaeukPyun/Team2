483bb2554ef40802ec9a7664481f016cf1fd3835
HaeukPyun <godnr149@gmail.com>
2020-07-15 00:28:47 +0900

    add.아이콘이미지
    시계앱 기본사진 추가.

--- summary_proj/project_front/js/iconStarters.js
+++ summary_proj/project_front/js/iconStarters.js
@@ -1,3 +1,4 @@
 createIcon('테트리스','imgs/Ticon.png','Tetris/index.html');
 createIcon('날씨정보','imgs/weatherSmallIcon.png','Weather/weather.html');
 createIcon('짝맞추기','../project_front/puzzle/img/k0_origin.png','../project_front/puzzle/main.html');
+createIcon("시계앱",'../project_front/imgs/clock.png','../project_front/clocks/Homework.index.html');
