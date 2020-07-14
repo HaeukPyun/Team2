ab709a166c43ca6069298916aae7f3ee824783a4
HaeukPyun <godnr149@gmail.com>
2020-07-15 01:27:11 +0900

    업데이트
    벽돌깨기 추가,
    인터넷이미지 변경

--- summary_proj/project_front/js/iconStarters.js
+++ summary_proj/project_front/js/iconStarters.js
@@ -2,3 +2,5 @@ createIcon('테트리스','imgs/Ticon.png','Tetris/index.html');
 createIcon('날씨정보','imgs/weatherSmallIcon.png','Weather/weather.html');
 createIcon('짝맞추기','../project_front/puzzle/img/k0_origin.png','../project_front/puzzle/main.html');
 createIcon("시계앱",'../project_front/imgs/clock.png','../project_front/clocks/Homework.index.html');
+createIcon('벽돌깨기','../project_front/imgs/blockImg.png','../project_front/blockBreak/break.html');
+
