c5e795fdc8f8f0c6b1b89a433f32723399defc99
HaeukPyun <godnr149@gmail.com>
2020-07-16 19:48:30 +0900

    갤러리 관련 업데이트
    갤러리 아이콘 추가 및,
    슬라이드 시간 2초에서 3.5초로 변경

--- summary_proj/project_front/js/iconStarters.js
+++ summary_proj/project_front/js/iconStarters.js
@@ -3,4 +3,4 @@ createIcon('날씨정보','imgs/weatherSmallIcon.png','Weather/weather.html');
 createIcon('짝맞추기','../project_front/puzzle/img/k0_origin.png','../project_front/puzzle/main.html');
 createIcon("시계앱",'../project_front/imgs/clock.png','../project_front/clocks/Homework.index.html');
 createIcon('벽돌깨기','../project_front/imgs/blockImg.png','../project_front/blockBreak/break.html');
-
+createIcon('갤러리','../project_front/bg_slide/imgs/gallery.jpg','../project_front/bg_slide/bg_note.html');
